55b61ace0f54c61bd8e9bb9c6b2c0d490f451967
AlleksV <82467373+AlleksV@users.noreply.github.com>
2022-04-08 23:05:34 +0100

    a

--- Switch.java
+++ Switch.java
@@ -1,11 +1,13 @@
 package main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Switch {
     public static void main (String [] args) throws IOException {
-        int month;
-        month = 5;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input number of the month: ");
+        int month = input.nextInt();
 
 
         String monthString;
@@ -17,6 +19,8 @@ public class Switch {
             case 3: monthString = "март";
             break;
             case 4: monthString = "апрель";
+            break;
+            case 5: monthString = "may";
             break;
             default:monthString = "не знаем такого :)";
 
